eaa9ea3a31570349b4688280eb36625c0a5a44d3
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-10 13:36:39 +0000

    Create add recipe view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Recipe
 
 class RecipeView(ListView):
@@ -9,3 +9,8 @@ class RecipeView(ListView):
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = 'recipe_detail.html'
+
+class AddRecipeView(CreateView):
+    model = Recipe
+    template_name = 'add_recipe.html'
+    fields = '__all__'
