abc554a5fc8622c7994dfc615abfc3e62f2daf54
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-10 14:36:39 +0000

    Add url path for updating recipe

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.RecipeView.as_view(), name='recipe_view'),
     path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('add_recipe/', views.AddRecipeView.as_view(), name='add_recipe'),
+    path('recipe/edit/<int:pk>', views.UpdateRecipeView.as_view(), name='update_recipe'),
 ]
