e6dc3f3b9556567c222c2ebfe2519a3e34fd6114
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-10 11:43:48 +0000

    Add recipe detail view

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Recipe
 class RecipeView(ListView):
     model = Recipe
     template_name = 'base.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_detail.html'
