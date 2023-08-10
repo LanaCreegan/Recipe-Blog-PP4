69080bcef11c97413300b0a4cd574183145aafd5
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-10 14:54:01 +0000

    Add url path to delete recipe

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('add_recipe/', views.AddRecipeView.as_view(), name='add_recipe'),
     path('recipe/edit/<int:pk>', views.UpdateRecipeView.as_view(), name='update_recipe'),
+    path('recipe/<int:pk>/delete', views.DeleteRecipeView.as_view(), name='delete_recipe'),
 ]
